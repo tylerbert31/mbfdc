4915888fbbff464d71a433e582f7a337de692871
Tyler Bert Baring <101559620+tylerbert31@users.noreply.github.com>
2023-10-05 11:26:22 +0800

    Removed default CRUD Pages

--- app/Controller/UsersController.php
+++ app/Controller/UsersController.php
@@ -36,8 +36,7 @@ class UsersController extends AppController
 	 */
 	public function index()
 	{
-		$this->User->recursive = 0;
-		$this->set('users', $this->Paginator->paginate());
+		return $this->redirect(array('action' => 'home'));
 	}
 
 	/**
@@ -47,34 +46,14 @@ class UsersController extends AppController
 	 * @param string $id
 	 * @return void
 	 */
-	public function view($id = null)
-	{
-		if (!$this->User->exists($id)) {
-			throw new NotFoundException(__('Invalid user'));
-		}
-		$options = array('conditions' => array('User.' . $this->User->primaryKey => $id));
-		$this->set('user', $this->User->find('first', $options));
-	}
+
 
 	/**
 	 * add method
 	 *
 	 * @return void
 	 */
-	public function add()
-	{
-		if ($this->request->is('post')) {
 
-			$this->User->create();
-
-			if ($this->User->save($this->request->data)) {
-				$this->Flash->success(__('The user has been saved.'));
-				return $this->redirect(array('controller' => 'users', 'action' => 'login'));
-			} else {
-				$this->Flash->error(__('The user could not be saved. Please, try again.'));
-			}
-		}
-	}
 
 	/**
 	 * edit method
@@ -83,23 +62,7 @@ class UsersController extends AppController
 	 * @param string $id
 	 * @return void
 	 */
-	public function edit($id = null)
-	{
-		if (!$this->User->exists($id)) {
-			throw new NotFoundException(__('Invalid user'));
-		}
-		if ($this->request->is(array('post', 'put'))) {
-			if ($this->User->save($this->request->data)) {
-				$this->Flash->success(__('The user has been saved.'));
-				return $this->redirect(array('action' => 'index'));
-			} else {
-				$this->Flash->error(__('The user could not be saved. Please, try again.'));
-			}
-		} else {
-			$options = array('conditions' => array('User.' . $this->User->primaryKey => $id));
-			$this->request->data = $this->User->find('first', $options);
-		}
-	}
+
 
 	/**
 	 * delete method
@@ -108,19 +71,7 @@ class UsersController extends AppController
 	 * @param string $id
 	 * @return void
 	 */
-	public function delete($id = null)
-	{
-		if (!$this->User->exists($id)) {
-			throw new NotFoundException(__('Invalid user'));
-		}
-		$this->request->allowMethod('post', 'delete');
-		if ($this->User->delete($id)) {
-			$this->Flash->success(__('The user has been deleted.'));
-		} else {
-			$this->Flash->error(__('The user could not be deleted. Please, try again.'));
-		}
-		return $this->redirect(array('action' => 'index'));
-	}
+
 
 	public function register()
 	{
